f4cf2bf04f9f5b0068044ec24c61941d54b757c7
崔晋瑜 <2510271615@qq.com>
2017-12-28 17:41:42 +0800

    Add files via upload

--- utils/utils.js
+++ utils/utils.js
@@ -16,5 +16,34 @@ module.exports = {
      */
     getNeighbor:function(target,id){
         return Buffer.concat([target.slice(0,10),id.slice(10)]);
+    },
+
+    /*
+        解析收到的Node    
+    */
+    decodeNode:function(nodes){
+        let arr = [];
+        console.log(nodes.length);
+        //node的长度应该是26，那么nodes的长度应是26的倍数
+        if(nodes.length %26 !== 0){
+            return arr
+        }
+
+        for(let i = 0;i+26<=nodes.length;i+=26){
+            arr.push({
+                nodeId: nodes.slice(i,i+20),
+                address: `${nodes[i+20]}.${nodes[i+21]}.${nodes[i+22]}.${nodes[i+23]}`,
+                port:nodes.readUInt16BE(i+24)
+            });
+        }
+
+        return arr;
+    },
+
+    /*
+        将Node转换为二进制
+    */
+    encodeNode:function(node){
+
     }
 }
