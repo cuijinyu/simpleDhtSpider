434e867ae226138be0268d6edbaa5501f90b5dff
崔晋瑜 <2510271615@qq.com>
2017-12-28 22:03:53 +0800

    the KTable
    completed the table and the bucket

--- spider/Bucket.js
+++ spider/Bucket.js
@@ -9,26 +9,50 @@ class Bucket{
         this.lastAccessed = Date.now(); //最后访问时间
     }
 
+    /**
+     * 向桶内插入node
+     * @param node
+     * @returns {string}
+     */
     append(node){
         if( node.nodeId > this.min && node.nodeId < this.max ){
-
+            node.nodeId = node.nodeId.toString();
+            if( node.nodeId.length != 20){
+                console.log("I am not equal to 20")
+                return
+            }
+            if(this.nodes.length == 0){
+                this.nodes.push(node);
+                return
+            }
             if(this.nodes.length < 8){
-
-                if( node.nodeId.length != 20){
-                    for(let i = 0;i < this.nodes.length;i++){
-                        if(this.nodes[i].nodeId == node.nodeId){
-                            this.nodes[i] = node;
-                        }else{
-                            this.nodes.push(node);
-                        }
+                for(let i = 0;i < this.nodes.length;i++){
+                    if(this.nodes[i].nodeId == node.nodeId){
+                        this.nodes[i] = node;
+                    }else{
+                        this.nodes.push(node);
                     }
                 }
-
             }else{
                 return "FULL";
             }
 
         }
     }
+
+    /**
+     * 判断node的ID是否在桶的范围内
+     * @param nodeId
+     * @returns {boolean}
+     */
+    nodeInRange(nodeId){
+        if(nodeId > this.min && nodeId < this.max){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
+let test = new Bucket(0,2E160);
+console.log(test.nodes);
 module.exports = Bucket;
--- spider/Table.js
+++ spider/Table.js
@@ -14,12 +14,61 @@ class Table{
     }
 
     /**
-     * 暂时还没有实现，之后实现
+     * 讲node插入到表中
      */
-    append(){
+    append(node){
+        if(node.nodeId == this.nodeId){
+            return ;
+        }else{
+            let index = this.getBucketIndex(node.nodeId);
+            let tempBucket = this.bucket[index];
+            let message = this.bucket[index].append(node);
+            if(message == "FULL"){
+                if(!this.bucket[index].nodeInRange(node.nodeId)){
+                    return;
+                }else{
+                    this.splitBucket(index);
+                    this.append(node);
+                }
+            }
+        }
+    }
 
+    /**
+     * 获取桶的引导
+     * @param nodeId
+     * @returns {number}
+     */
+    getBucketIndex(nodeId){
+        for(let i = 0;i < this.bucket.length;i ++){
+            if(this.bucket[i].nodeInRange(nodeId)){
+                return i;
+            }
+        }
+    }
+
+    /**
+     * 拆分桶
+     * @param index
+     */
+    splitBucket(index){
+        let oldBucket = this.bucket[index];
+        let point = this.bucket[index].max - (this.bucket[index].max - this.bucket[index].min)/2;
+        let newBucket = new Bucket(point,oldBucket.max);
+        oldBucket.max = point;
+        this.bucket.push(newBucket);
+        for(let node in newBucket){
+            if(node.nodeInRange(node.nodeId)){
+                newBucket.push(node);
+            }
+        }
+        for(let node in newBucket){
+            for(let i = 0;i<oldBucket.length;i++){
+                if(node.nodeId == oldBucket[i].nodeId){
+                    oldBucket.splice(i,1);
+                }
+            }
+        }
     }
 }
-var test = new Table();
-
 module.exports = Table;
