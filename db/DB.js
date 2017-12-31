7748d6ce84150b2dda2a27bf02c5d6ba90f0d64b
崔晋瑜 <2510271615@qq.com>
2017-12-31 15:40:57 +0800

    db config

---
+++ db/DB.js
@@ -0,0 +1,23 @@
+/**
+ * Created by 崔晋瑜 on 2017/12/31.
+ */
+let config=require("./dbconfig");
+let mysql=require("mysql");
+let pool=mysql.createPool(config.mysql);
+//sql语句执行
+function query(sql,callback) {
+    return new Promise((resolve,reject)=>{
+        pool.getConnection((err,connection)=>{
+            connection.query(sql,(err,rows)=>{
+                if(err){
+                    connection.end();
+                    reject(err);
+                }else {
+                    connection.end();
+                    resolve(rows);
+                }
+            })
+        })
+    })
+}
+exports.query=query;
---
+++ db/dbconfig.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 崔晋瑜 on 2017/12/31.
+ */
+module.exports={
+    mysql:{
+        host:'127.0.0.1',
+        user:'root',
+        password:'',
+        database:'dht',
+        port:3306
+    }
+}
