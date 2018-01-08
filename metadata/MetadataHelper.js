a66ff72c6c03ae638d5a25aa049c80968270e6fb
崔晋瑜 <2510271615@qq.com>
2018-01-08 19:56:25 +0800

    Add files via upload

--- metadata/MetadataHelper.js
+++ metadata/MetadataHelper.js
@@ -10,16 +10,53 @@ class MetadataHelper{
         this.infoHash = "";
         this.peerId = utils.getNodeId();
     }
+    sendMessage(msg){
+        this.client.write(msg,status=>{
+
+        })
+    }
     getMetadata(infoHash,address){
-        this.client.connect(address.port,address.address,()=>{
-            console.log(`Connect to ${address.address}:${address.port} successful`);
-        });
-        this.client.write()
+        try{
+            this.client.connect(address.port,address.address,()=>{
+                console.log(`Connect to ${address.address}:${address.port} successful`);
+                this.sendHandShake();
+                this.sendExtHandShake();
+            });
+            this.client.on('data',data=>{
+                console.log(data.toString());
+            });
+            this.client.on('close',()=>{
+
+            });
+            this.client.on('end',()=>{
+
+            });
+            this.client.on('error',()=>{
+                console.log(`can't connect to ${address.address}:${address.port}`)
+            })
+        }catch (e){
+            console.log(e);
+        }
         this.infoHash = infoHash;
     }
     sendHandShake(){
-        let msg = "BitTorrent protocol";
+        let msg = "\x13BitTorrent protocol";
+        msg += "\x00\x00\x00\x00\x00\x10\x00\x00";
         msg += this.infoHash;
-        msg += this.peerId;
+        msg = new Buffer(msg);
+        msg = Buffer.concat([msg,this.peerId]);
+        this.sendMessage(msg);
+    }
+    sendExtHandShake(){
+        let msg = "\x14\x00";
+        msg = new Buffer(msg);
+        msg = Buffer.concat([msg,bencode.encode({
+            m:{
+                ut_metadata:1
+            }
+        })])
+        this.sendMessage(msg);
     }
 }
+// let test = new MetadataHelper().sendExtHandShake();
+module.exports = MetadataHelper;
